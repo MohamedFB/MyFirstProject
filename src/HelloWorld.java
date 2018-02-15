4df4041170d6867fe02ab4217b59311b75ea8451
MohamedFB <mohamed.benelhaj@ocsbstudent.ca>
2018-02-15 11:02:48 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello World");
+	    System.out.println("Welcome to Computer Science");
+        String name="Mohamed";
+        System.out.println("My name is "+name);
+        String friend="Brother";
+        int age=16;
+        int ageindays=age*365;
+        System.out.println("My best friend is my"+friend+"My age in days"+ageindays);
+       
+    
+        
+        
+	}
+
+}
